640b0b72b4cacba67a56b886b623c311b9070647
Amanda Stern <amanda.stern.73@gmail.com>
2022-09-19 20:11:17 -0400

    first commit

---
+++ data/students-data-2.js
@@ -0,0 +1,10 @@
+const students = [
+  {name: 'Amanda', annoying: true, favFruit: 'mango'},
+  {name: 'Steph', annoying: false, favFruit: 'banana'},
+  {name: 'Nate', annoying: false, favFruit: 'apple'},
+  {name: 'Maddie', annoying: false, favFruit: 'grapes'},
+]
+
+export {
+  students
+}
